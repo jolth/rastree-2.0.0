0eb909eed9cb3eff23959440203982f846c16bfc
jolth <jolthgs@gmail.com>
2017-05-31 15:47:23 -0500

    upgrade: horometer virtual

--- db.py
+++ db.py
@@ -466,7 +466,7 @@ def listingVehicleClient(client_id):
        >>> 
     """
     from web.db import sqlquote
-    return config.DB.query("""SELECT v.id, lp.gps_id, v.placa, lp.odometer, 
+    return config.DB.query("""SELECT v.id, lp.gps_id, v.placa, lp.odometer, vs.horometer, 
             lp.velocidad, lp.fecha, vs.motor, lp.ubicacion, lp.position, v.active
             FROM vehiculos v
             INNER JOIN clientes_vehiculos AS cv ON (cv.vehiculo_id = v.id)
