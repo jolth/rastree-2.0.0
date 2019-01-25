42ea22b2d51738463545e849bd5c1dc2f2cbc622
jolth <jolthgs@gmail.com>
2019-01-25 17:11:54 -0500

    comment URL link

--- static/user/js/action_common.js
+++ static/user/js/action_common.js
@@ -16,10 +16,10 @@ link_active = {
 
         $('nav a').each(function(){ 
                  var href = $(this).attr('href');
-                 console.log(href + '==' + path);
+                 //console.log(href + '==' + path);
                  if (path == href){
                     $(this).parent().addClass('active');
-                    console.log($(this).parent().addClass('active'));
+                    //console.log($(this).parent().addClass('active'));
                  } else {
                     $(this).parent().removeClass('active');
                  }
